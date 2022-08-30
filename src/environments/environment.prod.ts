5351b8b5188a22c7cec78bf5a803200ae86cdaa2
Deepak5597 <deepakbisht5597@gmail.com>
2022-08-30 10:46:49 +0530

    url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_HOST: "https://asa-todo-app-utility-service.azuremicroservices.io/task-service"
+  API_HOST: "https://asa-todo-app-api-gateway.azuremicroservices.io/task-service"
 };
