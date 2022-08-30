091c12a9884e21a3d98c7e7eb36b10422bdd7fc4
Deepak Bisht <32607176+Deepak5597@users.noreply.github.com>
2022-08-30 15:20:32 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_HOST: "https://asa-todo-app-api-gateway.azuremicroservices.io/task-service"
+  API_HOST: "https://task-management-app-api-gateway.azuremicroservices.io/task-service"
 };
